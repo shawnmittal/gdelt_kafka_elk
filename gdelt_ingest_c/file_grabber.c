e0393be24ba8adda3c0adff079e7071f7ac8aacd
Shawn Mittal <shawn.mittal@gmail.com>
2023-03-13 17:07:39 -0400

    Updated c code to download files based on feed file list

--- gdelt_ingest_c/file_grabber.c
+++ gdelt_ingest_c/file_grabber.c
@@ -4,7 +4,8 @@
 #include <curl/curl.h>
 
 char **download_feed_files(void);
-int download_files_from_feeds(char **feed_files);
+int download_file_from_url(char *filename, char *url);
+char **download_files_from_feeds(char **feed_files);
 
 int main(void)
 {
@@ -16,6 +17,12 @@ int main(void)
 
     download_files_from_feeds(returned_filenames);
 
+    // Delete the downloaded files
+    for (int i = 0; i < 2; ++i)
+    {
+        remove(returned_filenames[i]);
+    }
+
     // Free the memory allocated for the array of strings
     for (int i = 0; i < 2; ++i)
     {
@@ -26,6 +33,28 @@ int main(void)
     return 0;
 }
 
+int download_file_from_url(char *filename, char *url)
+{
+    CURL *curl;
+    FILE *file;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if (curl)
+    {
+        file = fopen(filename, "wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        fclose(file);
+
+        return 0;
+    }
+    return 1;
+}
+
 char **download_feed_files(void)
 {
     CURL *curl;
@@ -34,7 +63,7 @@ char **download_feed_files(void)
     char *feeds[] = {
         "http://data.gdeltproject.org/gdeltv2/lastupdate.txt",
         "http://data.gdeltproject.org/gdeltv2/lastupdate-translation.txt"};
-    char *file_names[] = {
+    char *filenames[] = {
         "lastupdate.txt",
         "lastupdate-translation.txt"};
 
@@ -43,31 +72,21 @@ char **download_feed_files(void)
 
     for (int i = 0; i < 2; ++i)
     {
-        curl = curl_easy_init();
-        if (curl)
-        {
-            fp = fopen(file_names[i], "wb");
-            curl_easy_setopt(curl, CURLOPT_URL, feeds[i]);
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-            res = curl_easy_perform(curl);
-            curl_easy_cleanup(curl);
-            fclose(fp);
+        download_file_from_url(filenames[i], feeds[i]);
 
-            // Allocate memory for the filename string and copy it
-            returned_filenames[i] = malloc(sizeof(char) * (strlen(file_names[i]) + 1));
-            strcpy(returned_filenames[i], file_names[i]);
-        }
+        // Allocate memory for the filename string and copy it
+        returned_filenames[i] = malloc(sizeof(char) * (strlen(filenames[i]) + 1));
+        strcpy(returned_filenames[i], filenames[i]);
     }
 
     return returned_filenames;
 }
 
-int download_files_from_feeds(char **feed_files)
+char **download_files_from_feeds(char **feed_files)
 {
     FILE *fp;
     char line[200];
-    char *url;
+    int file_counter = 0;
 
     for (int i = 0; i < 2; ++i)
     {
@@ -76,7 +95,6 @@ int download_files_from_feeds(char **feed_files)
         if (fp == NULL)
         {
             printf("Unable to open file.\n");
-            return 1;
         }
 
         while (fgets(line, sizeof(line), fp) != NULL)
@@ -89,7 +107,18 @@ int download_files_from_feeds(char **feed_files)
                 {
                     // Remove newline character from last token
                     token[strcspn(token, "\n")] = 0;
-                    printf("%s\n", token);
+
+                    // Extract the filename from the url
+                    char chunk[200];
+                    strcpy(chunk, token);
+                    // printf("%s\n", token);
+                    char *filename;
+                    filename = strrchr(chunk, '/');
+                    filename++;
+                    printf("%s\n", filename);
+                    file_counter++;
+
+                    download_file_from_url(filename, token);
                 }
                 token = strtok(NULL, " ");
             }
